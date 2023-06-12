699873e4be9b67b08f50a5f23c6c8f1ae8b29db4
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-06-11 20:15:17 -0400

    Insert Copyright text in Footer via JavaScript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log('hello');
+// Create a new date object
+const today = new Date();
+
+// Retrieve the current year
+const thisYear = today.getFullYear();
+
+// Select the <footer> element from the DOM
+const footer = document.querySelector('footer');
+
+// Create a new paragraph element
+const copyright = document.createElement('p');
+
+// Set the inner HTML of the copyright element
+copyright.innerHTML = `Copyright &copy; Amirhossein Olyaei ${thisYear}`;
+
+// Append the copyright element to the footer
+footer.appendChild(copyright);
