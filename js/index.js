a14e588fc29fdd0f6c3e628db1c08cf103185f5a
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-08-01 20:11:03 -0400

    Add drop down menu

--- js/index.js
+++ js/index.js
@@ -191,6 +191,27 @@ const handleScroll = () => {
 // Attach the scroll event listener
 window.addEventListener('scroll', handleScroll);
 
+
+// Drop down menu on mobile view
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
 // AJAX
 // const githubRequest = new XMLHttpRequest();
 // githubRequest.addEventListener("load", (event)=>{
